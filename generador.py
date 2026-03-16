3bf8ca23e75351317fce0e1eafd2cf855958d00a
luciano <luchoserra288@gmail.com>
2026-03-16 12:30:01 -0300

    docs: add documentation and usage messages to compose generator

--- generador.py
+++ generador.py
@@ -1,7 +1,9 @@
 import sys
 
 
-def generar_server():
+def generate_server():
+    """Generate the YAML configuration for the server container."""
+
     return """  server:
     container_name: server
     image: server:latest
@@ -15,7 +17,9 @@ def generar_server():
 """
 
 
-def generar_cliente(i):
+def generate_client(i):
+    """Generate the YAML configuration for a client container."""
+
     return f"""  client{i}:
     container_name: client{i}
     image: client:latest
@@ -31,7 +35,9 @@ def generar_cliente(i):
 """
 
 
-def generar_network():
+def generate_network():
+    """Generate the YAML configuration for the testing network."""
+
     return """networks:
   testing_net:
     ipam:
@@ -42,19 +48,36 @@ def generar_network():
 
 
 def main():
+    """
+    Generates a docker-compose with a server and N clients.
+    """
+
+    if len(sys.argv) != 3:
+        print("Usage: python3 mi-generador.py <output_file> <number_of_clients>")
+        sys.exit(1)
+
     archivo = sys.argv[1]
-    clientes = int(sys.argv[2])
+
+    try:
+        clientes = int(sys.argv[2])
+    except ValueError:
+        print("Error: number_of_clients must be an integer")
+        sys.exit(1)
+
+    print(f"Generating compose in '{archivo}' with {clientes} clients")
 
     with open(archivo, "w") as f:
         f.write("name: tp0\n")
         f.write("services:\n")
 
-        f.write(generar_server())
+        f.write(generate_server())
 
         for i in range(1, clientes + 1):
-            f.write(generar_cliente(i))
+            f.write(generate_client(i))
 
-        f.write(generar_network())
+        f.write(generate_network())
+
+    print("Compose generated correctly.")
 
 
 if __name__ == "__main__":
