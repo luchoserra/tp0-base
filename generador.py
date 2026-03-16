f8df544e250780f5b26b01958aafc5a48cca005c
luciano <luchoserra288@gmail.com>
2026-03-16 12:07:35 -0300

    refactor: improve docker compose generator structure

--- generador.py
+++ generador.py
@@ -1,38 +1,61 @@
 import sys
 
-archivo = sys.argv[1]
-clientes = int(sys.argv[2])
 
-with open(archivo, "w") as f:
-    f.write("name: tp0\n")
-    f.write("services:\n")
+def generar_server():
+    return """  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
 
-    f.write("  server:\n")
-    f.write("    container_name: server\n")
-    f.write("    image: server:latest\n")
-    f.write("    entrypoint: python3 /main.py\n")
-    f.write("    environment:\n")
-    f.write("      - PYTHONUNBUFFERED=1\n")
-    f.write("      - LOGGING_LEVEL=DEBUG\n")
-    f.write("    networks:\n")
-    f.write("      - testing_net\n\n")
+"""
 
-    for i in range(1, clientes + 1):
-        f.write(f"  client{i}:\n")
-        f.write(f"    container_name: client{i}\n")
-        f.write("    image: client:latest\n")
-        f.write("    entrypoint: /client\n")
-        f.write("    environment:\n")
-        f.write(f"      - CLI_ID={i}\n")
-        f.write("      - CLI_LOG_LEVEL=DEBUG\n")
-        f.write("    networks:\n")
-        f.write("      - testing_net\n")
-        f.write("    depends_on:\n")
-        f.write("      - server\n\n")
 
-    f.write("networks:\n")
-    f.write("  testing_net:\n")
-    f.write("    ipam:\n")
-    f.write("      driver: default\n")
-    f.write("      config:\n")
-    f.write("        - subnet: 172.25.125.0/24\n")
+def generar_cliente(i):
+    return f"""  client{i}:
+    container_name: client{i}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID={i}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+
+"""
+
+
+def generar_network():
+    return """networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+"""
+
+
+def main():
+    archivo = sys.argv[1]
+    clientes = int(sys.argv[2])
+
+    with open(archivo, "w") as f:
+        f.write("name: tp0\n")
+        f.write("services:\n")
+
+        f.write(generar_server())
+
+        for i in range(1, clientes + 1):
+            f.write(generar_cliente(i))
+
+        f.write(generar_network())
+
+
+if __name__ == "__main__":
+    main()
