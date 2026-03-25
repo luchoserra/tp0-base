2284fa5dfd9e776e7378dcdbf33c24b895b1df94
luciano <luchoserra288@gmail.com>
2026-03-25 15:03:58 -0300

    feat: implement lottery notification and winners query

--- client/common/client.go
+++ client/common/client.go
@@ -59,6 +59,7 @@ func (c *Client) createClientSocket() error {
 	log.Criticalf("action: connect | result: fail | client_id: %v | error: %v", c.config.ID, err)
 	return err
 }
+// Sends a single batch of bets over the given protocol and waits for an OpOK message.
 func (c *Client) sendBatch(protocol *Protocol, batch []string) error {
 	if err := protocol.SendBatch(batch); err != nil {
 		return err
