10bff06e8a9f27121a06406193c137298448464e
luciano <luchoserra288@gmail.com>
2026-03-25 14:27:10 -0300

    refactor: send all batches over a single persistent connection

--- client/common/client.go
+++ client/common/client.go
@@ -59,7 +59,6 @@ func (c *Client) createClientSocket() error {
 	log.Criticalf("action: connect | result: fail | client_id: %v | error: %v", c.config.ID, err)
 	return err
 }
-
 func (c *Client) sendBatch(protocol *Protocol, batch []string) error {
 	if err := protocol.SendBatch(batch); err != nil {
 		return err
